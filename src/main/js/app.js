59028ce4a246c34f3bf024733e76220d2606712b
dineshgpillai <dinesh.g.pillai@gmail.com>
2021-08-04 21:05:22 +0100

    added  more code to app.js

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,10 +4,15 @@
 const React = require('react'); // <1>
 const ReactDOM = require('react-dom'); // <2>
 const client = require('./client'); // <3>
+
+var root = '/api';
 // end::vars[]
 
 // tag::app[]
 class App extends React.Component { // <1>
+
+
+
 
 	constructor(props) {
 		super(props);
@@ -15,10 +20,29 @@ class App extends React.Component { // <1>
 	}
 
 	componentDidMount() { // <2>
-		client({method: 'GET', path: '/api/employees'}).done(response => {
-			this.setState({employees: response.entity._embedded.employees});
-		});
+		this.loadFromServer(this.state.pageSize);
 	}
+
+	loadFromServer(pageSize) {
+    	follow(client, root, [
+    		{rel: 'employees', params: {size: pageSize}}]
+    	).then(employeeCollection => {
+    		return client({
+    			method: 'GET',
+    			path: employeeCollection.entity._links.profile.href,
+    			headers: {'Accept': 'application/schema+json'}
+    		}).then(schema => {
+    			this.schema = schema.entity;
+    			return employeeCollection;
+    		});
+    	}).done(employeeCollection => {
+    		this.setState({
+    			employees: employeeCollection.entity._embedded.employees,
+    			attributes: Object.keys(this.schema.properties),
+    			pageSize: pageSize,
+    			links: employeeCollection.entity._links});
+    	});
+    }
 
 	render() { // <3>
 		return (
@@ -63,6 +87,81 @@ class Employee extends React.Component{
 	}
 }
 // end::employee[]
+
+class CreateDialog extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const newEmployee = {};
+		this.props.attributes.forEach(attribute => {
+			newEmployee[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+		});
+		this.props.onCreate(newEmployee);
+
+		// clear out the dialog's inputs
+		this.props.attributes.forEach(attribute => {
+			ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+		});
+
+		// Navigate away from the dialog to hide it.
+		window.location = "#";
+	}
+
+	onCreate(newEmployee) {
+    	follow(client, root, ['employees']).then(employeeCollection => {
+    		return client({
+    			method: 'POST',
+    			path: employeeCollection.entity._links.self.href,
+    			entity: newEmployee,
+    			headers: {'Content-Type': 'application/json'}
+    		})
+    	}).then(response => {
+    		return follow(client, root, [
+    			{rel: 'employees', params: {'size': this.state.pageSize}}]);
+    	}).done(response => {
+    		if (typeof response.entity._links.last !== "undefined") {
+    			this.onNavigate(response.entity._links.last.href);
+    		} else {
+    			this.onNavigate(response.entity._links.self.href);
+    		}
+    	});
+    }
+
+	render() {
+		const inputs = this.props.attributes.map(attribute =>
+			<p key={attribute}>
+				<input type="text" placeholder={attribute} ref={attribute} className="field"/>
+			</p>
+		);
+
+		return (
+			<div>
+				<a href="#createEmployee">Create</a>
+
+				<div id="createEmployee" className="modalDialog">
+					<div>
+						<a href="#" title="Close" className="close">X</a>
+
+						<h2>Create new employee</h2>
+
+						<form>
+							{inputs}
+							<button onClick={this.handleSubmit}>Create</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+}
+
+
 
 // tag::render[]
 ReactDOM.render(
