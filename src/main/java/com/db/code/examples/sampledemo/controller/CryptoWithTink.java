b8da0e62b974eca77bc7af26c1f8062ba263e75c
dineshgpillai <dinesh.g.pillai@gmail.com>
2021-08-10 20:35:25 +0100

    got it working with Objects

--- src/main/java/com/db/code/examples/sampledemo/controller/CryptoWithTink.java
+++ src/main/java/com/db/code/examples/sampledemo/controller/CryptoWithTink.java
@@ -3,14 +3,20 @@ package com.db.code.examples.sampledemo.controller;
 import com.google.crypto.tink.*;
 import com.google.crypto.tink.aead.AeadConfig;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.security.GeneralSecurityException;
 
 
 
 import com.google.crypto.tink.config.TinkConfig;
+import org.springframework.util.SerializationUtils;
 
 public class CryptoWithTink {
+
+
 
     public static void main(String[] args) throws Exception {
 
@@ -31,10 +37,46 @@ public class CryptoWithTink {
             byte[] decrypted = aead.decrypt(ciphertext, aad.getBytes());
             String s = new String(decrypted);
             System.out.println("Text:"+s);
+
+            //try for an Java object
+            Sample obj = new Sample();
+            obj.display();
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(obj);
+            oos.flush();
+            byte [] data = bos.toByteArray();
+
+            byte[] cipherObject = aead.encrypt(data, data);
+            System.out.println("Cipher Object:"+cipherObject.toString());
+            byte[] decryptedObject = aead.decrypt(cipherObject, data);
+
+            Sample newObj = (Sample)SerializationUtils.deserialize(decryptedObject);
+            newObj.display();
+
+
+
         } catch (GeneralSecurityException e) {
             System.out.println(e);
             System.exit(1);
         }
     }
 
+}
+
+class Sample implements Serializable {
+    public void display() {
+        System.out.println("This is a sample class");
+    }
+}
+
+class ObjectToByteArray {
+    public static void main(String args[]) throws Exception {
+        Sample obj = new Sample();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(obj);
+        oos.flush();
+        byte [] data = bos.toByteArray();
+    }
 }
