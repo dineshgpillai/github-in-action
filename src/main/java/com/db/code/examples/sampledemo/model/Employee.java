5fb9835485c8f9eca8e8f049eb744a68864a4447
dineshgpillai <dinesh.g.pillai@gmail.com>
2021-08-19 20:57:18 +0100

    fixed the css file

--- src/main/java/com/db/code/examples/sampledemo/model/Employee.java
+++ src/main/java/com/db/code/examples/sampledemo/model/Employee.java
@@ -15,23 +15,28 @@
  */
 package com.db.code.examples.sampledemo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 /**
  * @author Dinesh Pillai
  */
 // tag::code[]
-@Entity // <1>
+@Entity
 public class Employee {
 
-    private @Id @GeneratedValue Long id; // <2>
+    private @Id @GeneratedValue Long id;
     private String firstName;
     private String lastName;
     private String description;
+
+    private @Version @JsonIgnore Long version;
 
     private Employee() {}
 
@@ -49,13 +54,14 @@ public class Employee {
         return Objects.equals(id, employee.id) &&
                 Objects.equals(firstName, employee.firstName) &&
                 Objects.equals(lastName, employee.lastName) &&
-                Objects.equals(description, employee.description);
+                Objects.equals(description, employee.description) &&
+                Objects.equals(version, employee.version);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, firstName, lastName, description);
+        return Objects.hash(id, firstName, lastName, description, version);
     }
 
     public Long getId() {
@@ -90,6 +96,14 @@ public class Employee {
         this.description = description;
     }
 
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
@@ -97,7 +111,7 @@ public class Employee {
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", description='" + description + '\'' +
+                ", version=" + version +
                 '}';
     }
-}
-// end::code[]
+}// end::code[]
